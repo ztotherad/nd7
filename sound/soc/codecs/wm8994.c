8170b312802662a2c2a2d840082d9e60c68e2198
Andrei F <luxneb@gmail.com>
2012-09-09 19:59:56 +0200

    sound-soc: wm8994: Add voodoo_sound implementation Courtesy of Gökhan Moral of Siyahkernel
    Change-Id: Ifff46fce2abecc28c6063fdb8e8f442dfd2bd5e6
    
    Conflicts:
    	sound/soc/codecs/Makefile
    	sound/soc/codecs/wm8994.c
    
    	modified:   arch/arm/configs/t0_04_defconfig sound-soc: wm8994: Add voodoo_sound implementation
    	modified:   sound/soc/Kconfig sound-soc: wm8994: Add voodoo_sound implementation
    	new file:   sound/soc/codecs/Kconfig.voodoo sound-soc: wm8994: Add voodoo_sound implementation
    	modified:   sound/soc/codecs/Makefile sound-soc: wm8994: Add voodoo_sound implementation
    	new file:   sound/soc/codecs/tegrak_voodoo_sound.h sound-soc: wm8994: Add voodoo_sound implementation
    	modified:   sound/soc/codecs/wm8994.c sound-soc: wm8994: Add voodoo_sound implementation
    	new file:   sound/soc/codecs/wm8994_voodoo.c sound-soc: wm8994: Add voodoo_sound implementation
    	new file:   sound/soc/codecs/wm8994_voodoo.h sound-soc: wm8994: Add voodoo_sound implementation

--- sound/soc/codecs/wm8994.c
+++ sound/soc/codecs/wm8994.c
@@ -201,6 +201,10 @@ int wm8994_volatile(struct snd_soc_codec *codec, unsigned int reg)
 	}
 }
 
+#ifdef CONFIG_SND_VOODOO
+#include "wm8994_voodoo.h"
+#endif
+
 static int wm8994_write(struct snd_soc_codec *codec, unsigned int reg,
 	unsigned int value)
 {
@@ -212,8 +216,13 @@ static int wm8994_write(struct snd_soc_codec *codec, unsigned int reg,
 	value = Boeffla_sound_hook_wm8994_write(reg, value);
 #endif
 
+
 #ifdef CONFIG_SND_WOLFSON_SOUND_CONTROL
 	value = sound_control_hook_wm8994_write(reg, value);
+
+#ifdef CONFIG_SND_VOODOO
+	value = voodoo_hook_wm8994_write(codec, reg, value);
+
 #endif
 
 	if (!wm8994_volatile(codec, reg)) {
@@ -4261,7 +4270,12 @@ static int wm8994_codec_probe(struct snd_soc_codec *codec)
 					ARRAY_SIZE(wm8958_intercon));
 		break;
 	}
-	
+
+#ifdef CONFIG_SND_VOODOO
+	voodoo_hook_wm8994_pcm_probe(codec);
+#endif
+
+
 #ifdef CONFIG_SND_BOEFFLA
 	Boeffla_sound_hook_wm8994_pcm_probe(codec);
 #endif
