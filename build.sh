b12ae663f9bf4a44b04d1827c485d341fcb1d80e
ztotherad <zrad.zg@gmail.com>
2014-07-27 06:47:14 -0500

    commented out linaro-4.9.1 toolchain and added gcc-4.7

--- build.sh
+++ build.sh
@@ -5,16 +5,17 @@ KERNEL_PATH=$PWD
 
 # Set toolchain path
 if [ "$(whoami)" == "ztotherad" ]; then
+	 TOOLCHAIN_PATH="/home/ztotherad/toolchains/gcc-4.7/bin"
 	#TOOLCHAIN_PATH="/home/ztotherad/toolchains/arm-eabi-4.4.3/bin"
 	#TOOLCHAIN_PATH="/home/ztotherad/toolchains/linaro-4.7.3/bin"
-	TOOLCHAIN_PATH="/home/ztotherad/toolchains/linaro-4.9.1/bin"
+	#TOOLCHAIN_PATH="/home/ztotherad/toolchains/linaro-4.9.1/bin"
 elif [ "$(whoami)" == "rollus" ]; then
 	TOOLCHAIN_PATH="/home/rollus/toolchains/arm-eabi-4.4.3/bin"
 
 fi
 
-TOOLCHAIN="$TOOLCHAIN_PATH/arm-eabi-"
-#TOOLCHAIN="$TOOLCHAIN_PATH/arm-linux-androideabi-"
+#TOOLCHAIN="$TOOLCHAIN_PATH/arm-eabi-"
+TOOLCHAIN="$TOOLCHAIN_PATH/arm-linux-androideabi-"
 MODULES="$KERNEL_PATH/ramdisk/lib/modules"
 ROOTFS_PATH="$KERNEL_PATH/ramdisk"
 
