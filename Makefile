dbe69a981217ab12f0252c8577b5a1b730aec89a
ztotherad <zrad.zg@gmail.com>
2014-07-31 07:07:27 -0500

    added arm-eabi-4.9/4.10/4.8 toolchains to build script, added arm-eabi-4.8 toolchain to Makefile

--- Makefile
+++ Makefile
@@ -193,8 +193,7 @@ SUBARCH := $(shell uname -m | sed -e s/i.86/i386/ -e s/sun4u/sparc64/ \
 # Note: Some architectures assign CROSS_COMPILE in their arch/*/Makefile
 export KBUILD_BUILDHOST := $(SUBARCH)
 ARCH		?=arm
-CROSS_COMPILE	?=/home/ztotherad/toolchains/gcc-4.7/bin/arm-linux-androideabi-
-
+CROSS_COMPILE	?=/home/ztotherad/toolchains/arm-eabi-4.8/bin/arm-eabi-
 # Architecture as present in compile.h
 UTS_MACHINE 	:= $(ARCH)
 SRCARCH 	:= $(ARCH)
--- build.sh
+++ build.sh
@@ -5,17 +5,18 @@ KERNEL_PATH=$PWD
 
 # Set toolchain path
 if [ "$(whoami)" == "ztotherad" ]; then
-	TOOLCHAIN_PATH="/home/ztotherad/toolchains/gcc-4.7/bin"
 	#TOOLCHAIN_PATH="/home/ztotherad/toolchains/arm-eabi-4.4.3/bin"
-	#TOOLCHAIN_PATH="/home/ztotherad/toolchains/linaro-4.7.3/bin"
+	TOOLCHAIN_PATH="/home/ztotherad/toolchains/arm-eabi-4.8/bin"
+	#TOOLCHAIN_PATH="/home/ztotherad/toolchains/arm-eabi-4.10/bin"
+	#TOOLCHAIN_PATH="/home/ztotherad/toolchains/arm-eabi-4.9/bin"
 	#TOOLCHAIN_PATH="/home/ztotherad/toolchains/linaro-4.9.1/bin"
 elif [ "$(whoami)" == "rollus" ]; then
 	TOOLCHAIN_PATH="/home/rollus/toolchains/arm-eabi-4.4.3/bin"
 
 fi
 
-#TOOLCHAIN="$TOOLCHAIN_PATH/arm-eabi-"
-TOOLCHAIN="$TOOLCHAIN_PATH/arm-linux-androideabi-"
+TOOLCHAIN="$TOOLCHAIN_PATH/arm-eabi-"
+#TOOLCHAIN="$TOOLCHAIN_PATH/arm-linux-androideabi-"
 MODULES="$KERNEL_PATH/ramdisk/lib/modules"
 ROOTFS_PATH="$KERNEL_PATH/ramdisk"
 
