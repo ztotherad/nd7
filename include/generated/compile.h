bf9609e5e96c8bf50fcefad82fac85f823adde2b
ztotherad <zrad.zg@gmail.com>
2014-07-31 10:48:15 -0500

    modified:   arch/arm/boot/compressed/Makefile initial commit 	modified:   include/generated/compile.h initial commit

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
 /* This file is auto generated, version 1 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#1 SMP PREEMPT Tue Jul 29 19:04:39 CDT 2014"
+#define UTS_VERSION "#1 SMP PREEMPT Thu Jul 31 07:04:00 CDT 2014"
 #define LINUX_COMPILE_BY "ztotherad"
 #define LINUX_COMPILE_HOST "archbng"
-#define LINUX_COMPILER "gcc version 4.7 (GCC) "
+#define LINUX_COMPILER "gcc version 4.8.3 20140426 (prerelease) (UBER-SaberMod-4.8.3-ARM-Graphite) "
