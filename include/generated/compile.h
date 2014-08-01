d5be26c0a0403b1a2f84925fb37dcf0390532d5b
ztotherad <zrad.zg@gmail.com>
2014-08-01 18:58:52 -0500

    initial commit

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 SMP PREEMPT Fri Aug 1 18:51:49 CDT 2014"
+#define LINUX_COMPILE_BY "ztotherad"
+#define LINUX_COMPILE_HOST "archbng"
+#define LINUX_COMPILER "gcc version 4.9.1 20140427 (prerelease) (UBER-SaberMod-4.9.1-ARM-Graphite) "
