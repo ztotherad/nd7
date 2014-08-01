f1603b30d79e01e4aa93acb4910f639b8a56dd20
ztotherad <zrad.zg@gmail.com>
2014-08-01 09:33:46 -0500

    modified:   Makefile added linaro 4.7.4 toolchain 	modified:   build.sh added linaro 4.7.4 toolchain & set as default 	modified:   include/config/kernel.release initial commit 	modified:   include/generated/compile.h initial commit 	modified:   include/generated/utsrelease.h initial commit

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 1 */
+/* This file is auto generated, version 2 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#1 SMP PREEMPT Thu Jul 31 07:04:00 CDT 2014"
+#define UTS_VERSION "#2 SMP PREEMPT Fri Aug 1 09:03:16 CDT 2014"
 #define LINUX_COMPILE_BY "ztotherad"
 #define LINUX_COMPILE_HOST "archbng"
-#define LINUX_COMPILER "gcc version 4.8.3 20140426 (prerelease) (UBER-SaberMod-4.8.3-ARM-Graphite) "
+#define LINUX_COMPILER "gcc version 4.7.4 20140402 (prerelease) (crosstool-NG linaro-1.13.1+bzr2651 - Linaro GCC 2014.04 - Cortex-A9) "
