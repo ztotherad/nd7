b88d8718f4c6cff8f3e3f2b3071c4c70bc39e7e8
ztotherad <zrad.zg@gmail.com>
2014-08-06 18:06:53 -0500

    added clean up script

---
+++ clean.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# set default path
+KERNEL_PATH=$PWD
+
+TOOLCHAIN="/home/ztotherad/toolchains/arm-eabi-4.9/bin/arm-eabi-"
+
+if [ -e boot.img ]; then
+	rm boot.img
+fi
+
+if [ -e compile.log ]; then
+	rm compile.log
+fi
+
+if [ -e ramdisk.cpio ]; then
+	rm ramdisk.cpio
+fi
+
+echo "Cleaning latest build"
+make ARCH=arm CROSS_COMPILE=$TOOLCHAIN -j`grep 'processor' /proc/cpuinfo | wc -l` mrproper
+echo "lastly, move *.zip to home directory"
+cd $KERNEL_PATH/release && mv *.zip $HOME
+echo "now removing release folder"
+rm -rf release
